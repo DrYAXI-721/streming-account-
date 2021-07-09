d0e14171c6d45aca0d3f41748547e39c1705dd98
DrYAXI-721 <83186549+DrYAXI-721@users.noreply.github.com>
2021-07-09 13:08:50 +0300

    Update and rename Yaxi.js to yaxi.js

--- yaxi.js
+++ yaxi.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const Yaxi = new Discord.Client();
 //////// playing or dnd  streaming ////////
-Yaxi.on("ready", () => {
+yaxi.on("ready", () => {
   console.log(
     `Online In Servers`
   );
@@ -13,16 +13,13 @@ Yaxi.on("ready", () => {
   ];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    Yaxi.user.setActivity(STREAMING, {      
+    yaxi.user.setActivity(STREAMING, {      
     type: "STREAMING", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
-///////////////////////
-Yaxi.on("ready", () => {
-    var join = Yaxi.channels.get(""); /// id voice
-    if (join) join.join();
-  });
+
 //////////////// tokn lera da bne 
-Yaxi.login("");
+
+yaxi.login("");
